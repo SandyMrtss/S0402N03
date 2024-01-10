f2e917ed074d733d60289f603f3adf02e49c6798
SandyMrtss <bebesubi2@hotmail.com>
2024-01-10 12:06:55 +0100

    structure change

--- src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t02/n03/model/repository/FruitRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.martos.sandra.s04.t02.n03.model.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FruitRepository<Fruit, Long> extends MongoRepository<Fruit, Long>{
-
-}
